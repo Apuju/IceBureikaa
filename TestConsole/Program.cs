fd0aacfb8f7f7244780243d4d08ed470b25381c9
apuju-aaronsu3-win10 <rainbowy0915@gmail.com>
2017-06-02 16:33:34 +0800

    Adds source

---
+++ TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IceBureikaa.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
